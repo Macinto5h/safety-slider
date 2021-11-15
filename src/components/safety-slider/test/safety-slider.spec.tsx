a20f7363eb994d329faa8e8f2260c0ad3402b652
Macallan Camara <mjcamara@protonmail.com>
2021-11-14 20:06:46 -0500

    Test: renders two arrows buttons when content is in the slot

--- src/components/safety-slider/test/safety-slider.spec.tsx
+++ src/components/safety-slider/test/safety-slider.spec.tsx
@@ -2,7 +2,7 @@ import { newSpecPage } from '@stencil/core/testing';
 import { SafetySlider } from '../safety-slider';
 
 describe('safety-slider', () => {
-  it('renders', async () => {
+  it('renders with no content', async () => {
     const page = await newSpecPage({
       components: [SafetySlider],
       html: `<safety-slider></safety-slider>`,
@@ -14,5 +14,12 @@ describe('safety-slider', () => {
         </mock:shadow-root>
       </safety-slider>
     `);
+  });
+  it('renders two arrow buttons when content is in the slot', async () => {
+    const page = await newSpecPage({
+      components: [SafetySlider],
+      html: `<safety-slider><img src="https://picsum.photos/100/100" alt="Randomly generated image"></safety-slider>`
+    });
+    expect(page.root.querySelectorAll('safety-slider__btn').length).toBe(2);
   });
 });
