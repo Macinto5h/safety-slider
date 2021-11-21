5f2ca97d81e77570b98ec4752b78933d1ae14181
Macallan Camara <mjcamara@protonmail.com>
2021-11-21 17:55:08 -0500

    Add autoprefixer

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,7 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
 
 export const config: Config = {
   namespace: 'safety-slider',
@@ -20,6 +22,9 @@ export const config: Config = {
     },
   ],
   plugins: [
-    sass()
+    sass(),
+    postcss({
+      plugins: [autoprefixer()]
+    })
   ]
 };
