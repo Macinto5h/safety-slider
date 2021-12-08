0a403c874f344ec90d89aa02094aebbdfce76930
Macallan Camara <mjcamara@protonmail.com>
2021-12-07 19:29:50 -0500

    Test: should emit the safetySliderDotClick event when a dot is clicked

--- src/components/safety-slider-dots/test/safety-slider-dots.e2e.ts
+++ src/components/safety-slider-dots/test/safety-slider-dots.e2e.ts
@@ -8,4 +8,17 @@ describe('safety-slider-dots', () => {
     const element = await page.find('safety-slider-dots');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('should emit the safetySliderDotClick event when a dot is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<safety-slider-dots dot-count="3"></safety-slider-dots>');
+
+    const eventSpy = await page.spyOnEvent('safetySliderDotClick');
+    const dotBtn = await page.find('button');
+
+    dotBtn.click();
+    await page.waitForEvent('safetySliderDotClick');
+
+    expect(eventSpy).toHaveReceivedEventDetail(0);
+  });
 });
