2143ad564677e5fb805aa1a62041b54895450b5c
Macallan Camara <mjcamara@protonmail.com>
2021-12-07 19:30:28 -0500

    Remove render test from safety-slider-dots

--- src/components/safety-slider-dots/test/safety-slider-dots.spec.tsx
+++ src/components/safety-slider-dots/test/safety-slider-dots.spec.tsx
@@ -2,17 +2,6 @@ import { newSpecPage } from '@stencil/core/testing';
 import { SafetySliderDots } from '../safety-slider-dots';
 
 describe('safety-slider-dots', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [SafetySliderDots],
-      html: `<safety-slider-dots></safety-slider-dots>`,
-    });
-    expect(page.root).toEqualHtml(`
-      <safety-slider-dots>
-      </safety-slider-dots>
-    `);
-  });
-
   it('renders a number of dot buttons based on the value passed to dotCount', async () => {
     const page = await newSpecPage({
       components: [SafetySliderDots],
