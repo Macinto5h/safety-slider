3a3611c57ac443ec5b6182dbe56922a1978c7945
Macallan Camara <mac@HPENVYx360Convertible.home>
2022-02-19 21:51:16 -0500

    cleanup safety slider spec test suite

--- src/components/safety-slider/test/safety-slider.spec.tsx
+++ src/components/safety-slider/test/safety-slider.spec.tsx
@@ -2,86 +2,44 @@ import { newSpecPage } from '@stencil/core/testing';
 import { SafetySlider } from '../safety-slider';
 import { SLIDER_ID_PREFIX } from '../safety-slider.resources';
 import { v4 as uuidv4 } from 'uuid';
+import { SpecUtils } from '../../../utils/spec-utils';
 
 describe('safety-slider', () => {
-  describe('property tests', () => {
-    it('does not render arrow buttons when the no-arrows property is present', async () => {
-      const page = await newSpecPage({
-        components: [SafetySlider],
-        html:
-          `<safety-slider no-arrows>
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          </safety-slider>`
-      });
+  it('does not render arrow buttons when the no-arrows property is present', async () => {
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData(), 'no-arrows');
 
-      expect(page.root.querySelectorAll(`.safety-slider-arrow`).length).toBe(0);
-    });
+    expect(SpecUtils.getArrowsComponent(page)).toBeNull();
+  });
 
-    it('does not render dot buttons when the no-dots property is present', async () => {
-      const page = await newSpecPage({
-        components: [SafetySlider],
-        html:
-          `<safety-slider no-dots>
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          </safety-slider>`
-      });
+  it('does not render dot buttons when the no-dots property is present', async () => {
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData(), 'no-dots');
 
-      expect(page.root.querySelectorAll(`.safety-slider-dot`).length).toBe(0);
-    });
+    expect(SpecUtils.getDotsComponent(page)).toBeNull();
+  });
 
-    it('should render the safety slider with a unique id', async () => {
-      const page = await newSpecPage({
-        components: [SafetySlider],
-        html:
-          `<safety-slider>
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          </safety-slider>`
-      });
+  it('should render the safety slider with a unique id', async () => {
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData());
 
-      const safetySliderId = page.root.id;
-      const uuidLength = 36;
+    const safetySliderId = page.root.id;
 
-      expect(safetySliderId.startsWith(SLIDER_ID_PREFIX)).toBeTruthy();
-      expect(safetySliderId.length).toEqual(SLIDER_ID_PREFIX.length + uuidLength);
-    });
+    expect(safetySliderId.startsWith(SLIDER_ID_PREFIX)).toBeTruthy();
+    expect(safetySliderId.length).toEqual(SLIDER_ID_PREFIX.length + uuidv4().length);
+  });
 
-    it('should set the uuid property of arrows, dots, and window to the same value', async () => {
-      const page = await newSpecPage({
-        components: [SafetySlider],
-        html:
-          `<safety-slider>
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-            <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          </safety-slider>`
-      });
+  it('should set the uuid property of arrows, dots, and window to the same value', async () => {
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData());
 
-      const sliderWindow = page.root.querySelector('safety-slider-window');
-      const sliderDots = page.root.querySelector('safety-slider-dots');
-      const sliderArrows = page.root.querySelector('safety-slider-arrows');
+    const sliderWindow = SpecUtils.getWindowComponent(page);
+    const sliderDots = SpecUtils.getDotsComponent(page);
+    const sliderArrows = SpecUtils.getArrowsComponent(page);
 
-      expect(sliderWindow.getAttribute('uuid').length).toEqual(uuidv4().length);
-      expect(sliderWindow.getAttribute('uuid')).toEqual(sliderDots.getAttribute('uuid'));
-      expect(sliderWindow.getAttribute('uuid')).toEqual(sliderArrows.getAttribute('uuid'));
-    });
+    expect(sliderWindow.getAttribute('uuid').length).toEqual(uuidv4().length);
+    expect(sliderWindow.getAttribute('uuid')).toEqual(sliderDots.getAttribute('uuid'));
+    expect(sliderWindow.getAttribute('uuid')).toEqual(sliderArrows.getAttribute('uuid'));
   });
 
   it('should change the active slide when setActiveSlide is called', async () => {
-    const page = await newSpecPage({
-      components: [SafetySlider],
-      html:
-        `<safety-slider>
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-        </safety-slider>`
-    });
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData());
 
     const safetySlider = page.rootInstance;
     safetySlider.setActiveSlide(1);
@@ -92,34 +50,20 @@ describe('safety-slider', () => {
   });
 
   it('should render with no dots and arrows if only one item is provided', async () => {
-    const page = await newSpecPage({
-      components: [SafetySlider],
-      html:
-        `<safety-slider>
-          <img src="https://picsum.photos/100/100/ alt="Randomly generated image">
-        </safety-slider>`
-    });
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData(1,1));
 
-    const arrows = page.root.querySelector('safety-slider-arrows');
-    const dots = page.root.querySelector('safety-slider-dots');
+    const arrows = SpecUtils.getArrowsComponent(page);
+    const dots = SpecUtils.getDotsComponent(page);
 
     expect(arrows).toBeNull();
     expect(dots).toBeNull();
   });
 
   it('should render with dots and arrows if there are more than one items in the slot', async () => {
-    const page = await newSpecPage({
-      components: [SafetySlider],
-      html:
-        `<safety-slider>
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-          <img src="https://picsum.photos/100/100" alt="Randomly generated image">
-        </safety-slider>`
-    });
+    const page = await SpecUtils.buildSliderSpecPage(SpecUtils.buildRandomSlotData(2));
 
-    const arrows = page.root.querySelector('safety-slider-arrows');
-    const dots = page.root.querySelector('safety-slider-dots');
+    const arrows = SpecUtils.getArrowsComponent(page);
+    const dots = SpecUtils.getDotsComponent(page);
 
     expect(arrows).not.toBeNull();
     expect(dots).not.toBeNull();
--- src/utils/spec-utils.ts
+++ src/utils/spec-utils.ts
@@ -1,7 +1,11 @@
 import { newSpecPage } from "@stencil/core/testing";
 import { SafetySliderArrows } from "../components/safety-slider-arrows/safety-slider-arrows";
+import { ARROWS_COMPONENT_QUERY } from "../components/safety-slider-arrows/safety-slider-arrows.resources";
 import { SafetySliderDots } from "../components/safety-slider-dots/safety-slider-dots";
+import { DOTS_COMPONENT_QUERY } from "../components/safety-slider-dots/safety-slider-dots.resources";
 import { SafetySliderWindow } from "../components/safety-slider-window/safety-slider-window";
+import { WINDOW_COMPONENT_QUERY } from "../components/safety-slider-window/safety-slider-window.resources";
+import { SafetySlider } from "../components/safety-slider/safety-slider";
 
 export class SpecUtils {
 
@@ -19,6 +23,13 @@ export class SpecUtils {
     });
   }
 
+  static buildSliderSpecPage(content = '', properties = '') {
+      return newSpecPage({
+          components: [SafetySlider],
+          html: `<safety-slider ${properties}>${content}</safety-slider>`
+      });
+  }
+
   static buildRandomSlotData(min = 0, max = 10) {
     let slotString = '';
     for (let i = 0; i < Math.round(Math.random() * (max - min)) + min; i++)
@@ -33,11 +44,23 @@ export class SpecUtils {
     });
   }
 
+  static getArrowsComponent(page) {
+    return page.root.querySelector(ARROWS_COMPONENT_QUERY);
+  }
+
+  static getDotsComponent(page) {
+    return page.root.querySelector(DOTS_COMPONENT_QUERY)
+  }
+
   static getNextArrowElement(page) {
     return page.root.querySelector('.safety-slider-arrow.-next');
   }
 
   static getPrevArrowElement(page) {
     return page.root.querySelector('.safety-slider-arrow.-prev');
+  }
+
+  static getWindowComponent(page) {
+    return page.root.querySelector(WINDOW_COMPONENT_QUERY);
   }
 }
