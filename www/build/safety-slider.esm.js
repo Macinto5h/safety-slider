eb63c6cca850668e573673a8881b94f2e6d5dd03
Macallan Camara <44030647+Macinto5h@users.noreply.github.com>
2022-03-27 22:14:37 -0400

    Add default values to documentation

--- www/build/safety-slider.esm.js
+++ www/build/safety-slider.esm.js
@@ -1,4 +1,4 @@
-import { B as BUILD, c as consoleDevInfo, p as plt, w as win, H, d as doc, N as NAMESPACE, a as promiseResolve, b as bootstrapLazy } from './index-2d4f7483.js';
+import { B as BUILD, c as consoleDevInfo, p as plt, w as win, H, d as doc, N as NAMESPACE, a as promiseResolve, b as bootstrapLazy } from './index-856ffa64.js';
 import { g as globalScripts } from './app-globals-0f993ce5.js';
 
 /*
