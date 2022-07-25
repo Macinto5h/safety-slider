4d27ff4a02c713dece32bea8dad0252a603dfb5c
Macallan Camara <44030647+Macinto5h@users.noreply.github.com>
2022-07-24 21:39:42 -0400

    Add new property no-swipe to safety-slider, docs, and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Ability to change slides by touch swipe
+- `no-swipe` property
+
 ### Changed
 
 ### Deprecated
--- src/components.d.ts
+++ src/components.d.ts
@@ -14,6 +14,7 @@ export namespace Components {
         "nextArrowAriaLabel": string;
         "nextArrowInnerHTML": string;
         "noDrag": boolean;
+        "noSwipe": boolean;
         "prevArrowAriaLabel": string;
         "prevArrowInnerHTML": string;
         "setActiveSlide": (newActiveSlide: number) => Promise<void>;
@@ -85,6 +86,7 @@ declare namespace LocalJSX {
         "nextArrowAriaLabel"?: string;
         "nextArrowInnerHTML"?: string;
         "noDrag"?: boolean;
+        "noSwipe"?: boolean;
         "prevArrowAriaLabel"?: string;
         "prevArrowInnerHTML"?: string;
         "trackTransitionDuration"?: number;
